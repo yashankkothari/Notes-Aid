6c29cfe862519b63f8e0a28df0d4d7de631f1f82
Aarush Saboo <160993404+aarushsaboo@users.noreply.github.com>
2025-02-19 16:09:46 +0530

    theme changing done, commented out fonts in layout.tsx for now because they were causing a build error.

--- notes-aid/src/app/layout.tsx
+++ notes-aid/src/app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes"
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Notes-Aid",
