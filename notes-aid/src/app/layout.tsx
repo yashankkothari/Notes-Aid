57e1c5eec6a6ffbea1615e17a23fd8f4893b31ea
MinavKaria <minavpkaria@gmail.com>
2025-02-22 15:59:20 +0530

    Add analytics

--- notes-aid/src/app/layout.tsx
+++ notes-aid/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes"
 import "./globals.css";
+import Metrics from "./metrics";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body>
         <ThemeProvider attribute="class">{children}</ThemeProvider>
+        <Metrics />
       </body>
     </html>
   )
