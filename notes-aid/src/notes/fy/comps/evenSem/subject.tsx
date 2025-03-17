94371500e6b34eadbb99a5c8c79d1a6a4d37f19b
MinavKaria <minavpkaria@gmail.com>
2025-03-17 09:12:40 +0530

    Add am1 and am2 in both semesters

--- notes-aid/src/notes/fy/comps/evenSem/subject.tsx
+++ notes-aid/src/notes/fy/comps/evenSem/subject.tsx
@@ -4,11 +4,14 @@ import { Subjects } from "@/interfaces/Subject";
 import am2 from './am2/data';
 import em from './em/data';
 import ep from './ep/data';
+import am1 from "../oddSem/am1/data";
 
 const subjects: Subjects = {
+    am1:am1,
     am2:am2,
     em:em,
     ep:ep,
 };
+
 
 export default subjects;
--- notes-aid/src/notes/fy/comps/oddSem/subjects.tsx
+++ notes-aid/src/notes/fy/comps/oddSem/subjects.tsx
@@ -9,12 +9,14 @@ import eeee from "./eeee/data";
 import ec from "./ec/data";
 import ed from "./ed/data";
 import am1 from "./am1/data";
+import am2 from "../evenSem/am2/data";
 
 const subjects: Subjects = {
   eeee: eeee,
   ec: ec,
   ed: ed,
   am1: am1,
+  am2: am2,
 };
 
 export default subjects;
