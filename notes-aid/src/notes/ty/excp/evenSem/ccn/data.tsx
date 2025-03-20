797456adfb690c6e490ba83f6844005dd816e818
PruthvirajZala22 <pruthviraj220407@gmail.com>
2025-03-20 21:35:31 +0530

    ccn test mod (#12)
    * add ccn notes mod 1
    
    * add ccn mod 1 test
    
    * add test ccn mode

--- notes-aid/src/notes/ty/excp/evenSem/ccn/data.tsx
+++ notes-aid/src/notes/ty/excp/evenSem/ccn/data.tsx
@@ -20,7 +20,18 @@ const ccn = {
           url: "https://somaiya0-my.sharepoint.com/:p:/g/personal/pruthvirajsinh_z_somaiya_edu/EQSfcG-NssNHnKX1_GyC1CMBl9riz29aqL5EuYaVGLPlig?e=jfSzZk"
         }
       ],
-      topics: [],
+      topics: [
+        {
+        title: "1.1.1 Introduction to digital signals and systems",
+        description: "Basic theoretical concepts",
+        videos: [
+          {
+            title: "Introduction to Digital Signals and Systems",
+            url: "https://www.youtube.com/embed/iCaDt9Esdv4?si=KFIJ7HSUSv-S6pRo",
+          },
+        ]
+      }
+    ],
     },
     2:{
       notesLink: [
