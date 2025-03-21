b52e98046a5b342c8f4bb7a837cc21d5d85ca220
Aksh Maheshwari <157402019+AkshMaheshwari@users.noreply.github.com>
2025-03-22 00:05:05 +0530

    Update AOA module 1 notes (#19)

--- notes-aid/src/notes/sy/comps/evenSem/aoa/data.tsx
+++ notes-aid/src/notes/sy/comps/evenSem/aoa/data.tsx
@@ -1,15 +1,146 @@
-import { Brain } from "lucide-react";
+import {
+  Signal,
+} from "lucide-react";
 
 const aoa = {
-  name: "Analysis of Algorithms",
-  icon: Brain,
-  color: "blue",
-  modules: {
-    1: {
-      notesLink: [],
-      topics: [],
-    },
+name: "Analysis of Algorithms",
+icon: Signal,
+color: "blue",
+modules: {
+  1: {
+    notesLink: [
+      {
+        title: "Handwritten Notes for Module 1",
+        url: "https://drive.google.com/file/d/17z1CWEZoU1WckKzN6jTfmdN5dwwsoSzV/view",
+      },
+      {
+        title: "Pdf Notes ",
+        url: "https://drive.google.com/file/d/1jjfJvs7_gqVbAEBeZYPI9e_DS-aBaCTU/view",
+      }
+    ],
+    topics: [
+      {
+        title: "1.1: Introduction to analysis of algorithms",
+        description: "Basic theoretical concepts",
+        videos: [
+          {
+            title: "1.1.1: Introduction to Algorithms",
+            url: "https://www.youtube.com/embed/0IAPZzGSbME?si=XdcefU7ShcuDQvmS",
+          },
+          {
+            title: "1.1.2: Priori Analysis and Posteriori Testing",
+            url: "https://www.youtube.com/embed/-JTq1BFBwmo?si=29koC6zl72lFnD9X",
+          }
+        ],
+        // notes: [
+        //   {
+        //     title: "Lecture Notes - Week 1",
+        //     url: "https://example.com/notes1.pdf",
+        //   },
+        //   {
+        //     title: "Practice Problems",
+        //     url: "https://example.com/practice1.pdf",
+        //   },
+        // ],
+      },
+      {
+        title: "1.2: Time and Space Complexity",
+        description: "Very Very Important Topic for Exam ⭐",
+        videos: [
+          {
+            title: "1.2.1: Time Complexity-1",
+            url: "https://www.youtube.com/embed/9TlHvipP5yA?si=t8VWHCifE8oCx7wa",
+          },
+          {
+            title: "1.2.2: Time Complexity-2",
+            url: "https://www.youtube.com/embed/9SgLBjXqwd4?si=uPIMTAIgq_6AY07G",
+          },
+          {
+            title: "1.2.3: Time Complexity-3",
+            url: "https://www.youtube.com/embed/p1EnSvS3urU?si=qRfBQe5WUMbMfDqX",
+
+          }
+          
+        ],
+      },
+      {
+        title: "1.3 Asymptotic Notations",
+        description: "Important Topic for Exam ⭐",
+        
+        videos: [
+          {
+            title: "1.3.1: Asymptotic Notations - Part 1",
+            url: "https://www.youtube.com/embed/A03oI0znAoc?si=-QQlUu8-aZaSHzQy",
+          },
+          {
+            title: "1.3.2: Asymtopotic Notations - Part 2",
+            url: "https://www.youtube.com/embed/Nd0XDY-jVHs?si=ug28pCmtQq555c50",
+            
+          },
+          {
+            title: "1.3.3: Properties of Asymptotic Notations",
+            url:"https://www.youtube.com/embed/NI4OKSvGAgM?si=V5GEEO9zMPguSAyu",
+          }
+          
+        ],
+      },
+      {
+        title: "1.4: Recurrence Relations for Decreasing Functions",
+        description: "Important Topic for Exam - Solving⭐",
+        videos: [
+          {
+            title: "1.4.1: Recurrence Relations - Part 1",
+            url: "https://www.youtube.com/embed/4V30R3I1vLI?si=aMtfCDwHXFd4LBm8",
+          },
+          {
+            title: "1.4.2: Recurrence Relations - Part 2",
+            url: "https://www.youtube.com/embed/IawM82BQ4II?si=czElVINhSHhGUp0K",
+          },
+          {
+            title: "1.4.3: Recurrence Relations - Part 3",
+            url: "https://www.youtube.com/embed/MhT7XmxhaCE?si=xdjs7k62XIbmJSfE",
+          },
+          {
+            title: "1.4.4: Recurrence Relations - Part 4",
+            url: "https://www.youtube.com/embed/JvcqtZk2mng?si=3XTYWq6XcweCPkpa",
+          },
+          {
+            title: "1.4.5: Masters Theorem for Decreasing Functions",
+            url: "https://www.youtube.com/embed/CyknhZbfMqc?si=8VLUJbjwAjLgRNjj",
+          }
+          
+        ],
+      },
+      {
+        title: "1.5 Recurrence Relations for Dividing Functions",
+        description: "Important Topic for Exam ⭐- Numerical based",
+        videos: [
+          {
+            title: "1.5.1: Recurrence Relations - Part 1",
+            url: "https://www.youtube.com/embed/8gt0D0IqU5w?si=YJrge8J6oH6Jx7FE",
+          },
+          {
+            title: "1.5.2: Recurrence Relations - Part 2",
+            url: "https://www.youtube.com/embed/XcZw01FuH18?si=btborgJZ3FyVaWw8",
+          },
+          {
+            title: "1.5.3: Recurrence Relations - Part 3",
+            url: "https://www.youtube.com/embed/1K9ebQJosvo?si=Tb_aMqRo0-NBrFOn",
+          },
+          {
+            title: "1.5.4: Masters Theorem for Dividing Functions",
+            url: "https://www.youtube.com/embed/OynWkEj0S-s?si=MFcZgq4EOAozPaDw",
+          },
+          {
+            title: "1.5.5: Masters Theorem : Numericals",
+            url: "https://www.youtube.com/embed/kGcO-nAm9Vc?si=Iwh5hiX_q5jwVyTo",
+          }
+        ],
+      },
+    ],
   },
+  
+},
 };
 
 export default aoa;
