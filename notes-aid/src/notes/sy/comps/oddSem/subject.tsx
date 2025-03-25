ac0eccaa99cf8c68d2b6aca95788925788fff197
Mihir Phalke <mihirphalke@gmail.com>
2025-03-25 19:37:29 +0530

    Added DSM notes for COMPS (#47)

--- notes-aid/src/notes/sy/comps/oddSem/subject.tsx
+++ notes-aid/src/notes/sy/comps/oddSem/subject.tsx
@@ -3,12 +3,14 @@ import coa from "./coa/data";
 import ds from "./ds/data";
 import itvc from "./itvc/data";
 import oopm from "./oopm/data";
+import dsm from "./dsm/data";
 
 const subjects: Subjects = {
-    coa: coa,
-    ds: ds,
-    itvc: itvc,
-    oopm: oopm,
+  coa: coa,
+  ds: ds,
+  itvc: itvc,
+  oopm: oopm,
+  dsm: dsm,
 };
 
 export default subjects;
